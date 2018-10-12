fa4c0cab98c4f3d80677c0ccb218ce3548ff3946
Spencer Reynolds <spencerr@us.ibm.com>
2018-10-11 19:18:16 -0500

    initial commit

---
+++ 01week/datatypes.js
@@ -0,0 +1,62 @@
+'use strict'
+//1. Write a JavaScript program to display the current day and time.
+const todaysDate = new Date();
+console.log(todaysDate);
+
+//2. Write a JavaScript program to convert a number to a string.
+
+const numberToString = (num1) => {
+console.log(Number(num1))
+}
+numberToString(22)
+
+//3. Write a JavaScript program to convert a string to the number.
+const string = '18';
+const integer = parseInt(string, 10);
+console.log(integer);
+
+
+//4. Write a JavaScript program that takes in different datatypes and prints out whether they are a: Boolean, Null, Undefined, Number, NaN, String
+
+const whatDataType = (dataType) => {
+console.log(typeof (dataType))
+}
+whatDataType(true)
+
+//5. Write a JavaScript program that adds 2 numbers together.
+
+const sumOfTwoNumbers = (num1, num2) => {
+  if (num1 && num2){
+    return num1 + num2
+    }
+  else {
+   return console.log('One or both of the arguments are not numbers')
+    }
+}
+
+
+//6. Write a JavaScript program that runs only when 2 things are true.
+
+const trueInputs = (input1, input2) => {
+  if (input1 && input2) {
+    console.log('both are true')
+  }
+}
+trueInputs(9,true);
+
+//7. Write a JavaScript program that runs when 1 of 2 things are true.
+
+const orInputs = (input1, input2) => {
+  if (input1 || input2)  {
+    console.log('perfect')
+  }
+}
+orInputs(5,'holler')
+//8.Write a JavaScript program that runs when both things are not true.
+
+const falseInputs = (input1, input2) => {
+  if (!input1 && !input2) {
+    console.log('both are false')
+  }
+}
+falseInputs(9,true);
