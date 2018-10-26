9b8a0d897ec9d792d53db4db3aae6a24cad3d454
Spencer Reynolds <spencerr@us.ibm.com>
2018-10-25 19:18:02 -0500

    first try, can't get for.each working =(

---
+++ 04week/gitOlympics.js
@@ -0,0 +1,12 @@
+'use strict'
+
+
+const listOfOlympians = ['Phelps', 'Biles', 'Jose', 'Spencer', 'Katie']
+
+
+const printListOfOlympians = (arr) =>{
+    arr.forEach(logIt(arr) {
+        console.log(arr);
+    });
+}
+printListOfOlympians(listOfOlympians);
