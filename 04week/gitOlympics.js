003d9a8fced0ddeda2ec97e9f65a7518653cc62b
Spencer Reynolds <spencerr@us.ibm.com>
2018-10-25 20:19:02 -0500

    notes on forEach

--- 04week/gitOlympics.js
+++ 04week/gitOlympics.js
@@ -15,3 +15,14 @@ printListOfOlympians(listOfOlympians);
 // arr.forEach((item,index)=> {
 //     console.log(item)
 // })
+
+
+//for each can only do arrays.
+
+//popular method for turning an object into an array, then using forEach on that.
+const details = starChart.details;
+const detailsKeys = Object.keys(details);
+
+detailsKeys.forEach((key, index)=>{
+    console.log(key, details[key])
+})
