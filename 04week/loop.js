f5bebac8fbcb5693cad62451d8ca988e2147033b
Spencer Reynolds <spencerr@us.ibm.com>
2018-11-03 08:21:30 -0500

    first pass

---
+++ 04week/loop.js
@@ -0,0 +1,18 @@
+
+const userInfo = {
+firstName: "Jane",
+lastName: "Doe",
+birthDate: "Jan 5, 1925",
+gender: "female"
+};
+
+
+const carsInReverse = ['Honda','Toyota','Porsche','Audi'];
+
+carsInReverse.forEach((car)=>{
+    console.log(car);
+});
+
+for (let i = 1; i<1001; i++) {
+          console.log(i);
+};
